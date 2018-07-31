a4da7aff5770d01b511618eb918e986b25f25af4
Nuzair <nuzairb@gmail.com>
2018-07-31 17:48:33 +0500

    Basic setup navbar section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { BoxshadowComponent } from './boxshadow/boxshadow.component';
+import { GradientComponent } from './gradient/gradient.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'gradient',
+    component: GradientComponent
+  },
+  {
+    path: 'box-shadow',
+    component: BoxshadowComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
